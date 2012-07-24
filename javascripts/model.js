608a7db90145a51a00951a943acf9a5951426eeb
Michael Aufreiter <ma@zive.at>
2012-07-24 00:49:53 -0400

    Use data from interior.substance.io.

--- javascripts/model.js
+++ javascripts/model.js
@@ -4,40 +4,7 @@
 // Sucks documents from documentcloud.org
 
 function loadDocuments(searchstr, facets, cb) {
-
-  // Remove to get the DocumentCloud stuff
-  return cb(null, new Data.Collection(countries_fixture));
-
-  if (searchstr == "all") searchstr = "";
-  $.ajax({
-    type: "GET",
-    url: 'https://www.documentcloud.org/api/search.json?per_page=100&order=score&q='+encodeURI(searchstr || "")+'%20group%3A+revenuewatch&data=true',
-    dataType: "jsonp",
-    success: function(res) {
-      if(res.error) return cb(res.error);
-      
-      var documents = {};
-      _.each(res.documents, function(doc) {
-        documents[doc.id] = {
-          "title": doc.title,
-          "image": "images/kevin.png",
-          "description": doc.description,
-          "countries": doc.data["countries"] ? doc.data["countries"].split(',') : [],
-          "individuals": doc.data["individuals"] ? doc.data["individuals"].split(',') : [],
-          "company": doc.data["company"] ? [doc.data["company"]] : [],
-          "agency": doc.data["agency"] ? [doc.data["agency"]] : [],
-          "updated_at": doc.updated_at,
-          "thumbnail": doc.resources.thumbnail,
-          "pdf": doc.resources.pdf,
-        }
-      });
-
-      cb(null, new Data.Collection(_.extend(seed, {"items": documents})));
-    },
-    error: function(err) {
-      cb(err);
-    }
-  });
+  return cb(null, new Data.Collection(documents_fixture));
 }
 
 // Sort facets by their document frequency
